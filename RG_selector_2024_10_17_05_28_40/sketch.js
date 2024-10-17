4b8eb052d5ed24aa5b1d6f5e0d0c77a86ddf5739
anf34 <58158072+anf34@users.noreply.github.com>
2024-10-17 16:30:03 +1100

    Add files via upload

---
+++ RG_selector_2024_10_17_05_28_40/sketch.js
@@ -0,0 +1,38 @@
+//RG colour selector :)
+
+//Next to Implement:
+
+//Instead of changing b by moving Y, introduce a slider.
+
+//When pressing on background, tells you the hex and
+//numbered value for that colour
+
+function setup() {
+  createCanvas(450, 450);
+}
+
+function draw() {
+  b = 255;
+  b = map(mouseY, 0, height, 0, 255);
+  noStroke();
+
+  s = 50; //square size
+
+  //The loop below draws squares for entire
+  //width of window.
+  i = 0;
+  while (i < width) {
+    //draws squares across the height of window
+    //for a given width
+    j = 0;
+    while (j < height) {
+      r = map(i, 0, width, 0, 255);
+      g = map(j, 0, height, 0, 255);
+      fill(r, g, b);
+      rect(i, j, s);
+      j += s;
+    }
+
+    i += s;
+  }
+}
