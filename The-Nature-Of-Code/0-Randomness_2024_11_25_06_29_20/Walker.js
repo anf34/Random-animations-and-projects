1ab1be2f2e37dda1627e24a5b14bab4ebd60e9b0
anf34 <58158072+anf34@users.noreply.github.com>
2024-11-25 17:30:09 +1100

    worked on randomness

---
+++ The-Nature-Of-Code/0-Randomness_2024_11_25_06_29_20/Walker.js
@@ -0,0 +1,57 @@
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+
+  show() {
+    //strokeWeight(1)
+    //fill(72, 247, 249)
+    fill(0);
+    ellipse(this.x, this.y, 1);
+
+    //stroke(0)
+    //point(this.x, this.y)
+  }
+
+  step() {
+    //0, 1, 2, or 3. The random choice determines the step.
+    let step = [1, 1, 1, 1];
+    //let x_step = 1;
+    //let y_step = x_step;
+    //let irregular_step = 1;
+    let probs = [0.25, 0.25, 0.25, 0.25];
+    let choice = random(1);
+
+    if (choice < 0.5) {
+      if (choice < 0.25) {
+        if (mouseX - this.x > 0) {
+          this.x += step[0];
+        } else {
+          this.x -= step[1];
+        }
+      } else {
+        if (mouseY - this.y > 0) {
+          this.y += step[2];
+        } else {
+          this.y -= step[3];
+        }
+      }
+      choice = 10;
+      //console.log("Closer to mouse!");
+    } else {
+      if (choice >= 0 && choice <= probs[0]) {
+        this.x += step[0];
+      } else if (choice <= probs[0] + probs[1]) {
+        this.x -= step[1];
+      } else if (choice <= probs[0] + probs[1] + probs[2]) {
+        this.y += step[2];
+      } else if (choice <= 1) {
+        this.y -= step[3];
+      } else {
+        print("Error");
+      }
+      //console.log("Random step!");
+    }
+  }
+}
---
+++ The-Nature-Of-Code/0-Randomness_2024_11_25_06_29_20/sketch.js
@@ -0,0 +1,34 @@
+//Up to exercise 0.3
+//Up to: clean this code up!!!!
+
+let walker;
+let i = 0;
+function setup() {
+  createCanvas(400, 400);
+  background(255,223,0);
+  walker = new Walker()
+}
+
+//The two questions that I’ll ask over and over again throughout this book: 
+//How do you define the rules that govern the behavior of your objects?
+//How do you implement these rules in code?
+
+function draw() {
+  /*if (i % 100 === 0){
+    walker = new Walker();
+  }*/
+  
+  /*
+  if (random(1) < 0.01){
+    print("Killed old walker.")
+    print("Lasted", i, "frames!")
+    walker = new Walker();
+    print("say hi to new guy")
+  }
+  */
+  
+  walker.show();
+  walker.step();
+  i += 1
+
+}
